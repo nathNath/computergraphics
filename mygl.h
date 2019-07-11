242028bc352e06048a14e5a9409fac84fc722888
nathnath <nathaliavasconcelos1@gmail.com>
2019-07-10 22:14:18 -0300

    PutPixel() working properly.

--- mygl.h
+++ mygl.h
@@ -6,9 +6,22 @@
 //*****************************************************************************
 // Defina aqui as suas funções gráficas
 //*****************************************************************************
+void PutPixel(uint x, uint y, uint r, uint g, uint b, uint a){
+    
+    FBptr[4*x + 4*y*IMAGE_HEIGHT + 0] = r;
+    FBptr[4*x + 4*y*IMAGE_HEIGHT + 1] = g;
+    FBptr[4*x + 4*y*IMAGE_HEIGHT + 2] = b;
+    FBptr[4*x + 4*y*IMAGE_HEIGHT + 3] = a;
+       
+}
 
+void DrawLine(){
 
+}
 
+void DrawTriangle(){
+
+}
 
 #endif // _MYGL_H_
 
